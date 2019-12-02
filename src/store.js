aac3e5388f5c5ba2720c3e9fc3fa8d821fd475be
Ha Nguyen <hanguyen.0096@gmail.com>
2019-12-02 00:02:01 -0500

    add database

---
+++ src/store.js
@@ -0,0 +1,19 @@
+const bookmarks = [
+  { id: 4,
+    title: 'Thinkful',
+    url: 'https://www.thinkful.com',
+    description: 'Think outside the classroom',
+    rating: 5 },
+  { id: 5,
+    title: 'Google',
+    url: 'https://www.google.com',
+    description: 'Where we find everything else',
+    rating: 4 },
+  { id: 6,
+    title: 'MDN',
+    url: 'https://developer.mozilla.org',
+    description: 'The only place to find web documentation',
+    rating: 5 },
+]
+
+module.exports = { bookmarks }
